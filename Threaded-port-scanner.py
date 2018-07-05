be5ae8d988b85cf45b409d1901dcfcddd218af97
Roks <pauloroks@gmail.com>
2018-07-05 14:10:33 -0300

    commit

---
+++ Threaded-port-scanner.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+import time
+from queue import Queue
+
+print_lock = threading.Lock()
+
+target = 'hackthissite.org'
+#ip = socket.gethostbyname(target)
+
+
+def portscan(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        con = s.connect((target,port))
+        with print_lock:
+            print('port',port)
+        con.close()
+    except:
+        pass
+
+def threader():
+    while True:
+        worker = que.get()
+
+        portscan(worker)
+
+        que.task_done()
+
+que = Queue()
+
+for x in range(30):
+    t = threading.Thread(target=threader)
+
+    t.daemon= True
+
+    t.start()
+
+start =time.time()
+
+for worker in range(1,100):
+    que.put(worker)
+
+que.join()
